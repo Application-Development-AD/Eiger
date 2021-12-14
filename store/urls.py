2d8d72974a9456b79f331145b7631141cce3c45d
Nida Damia <nidamianizam@gmail.com>
2021-12-14 09:23:26 +0800

    init

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.product, name="product"),
+    path('', views.search, name="search"),
     path('cart/', views.cart, name="cart"),
     path('product/', views.product, name="product"),
     
