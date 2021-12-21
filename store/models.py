19b10e2e9ded40899cde6aa5c74b6c91ab773b10
Nida Damia <nidamianizam@gmail.com>
2021-12-21 08:54:01 +0800

    init

--- store/models.py
+++ store/models.py
@@ -11,14 +11,42 @@ class Customer(models.Model):
 	def __str__(self):
 		return self.name
 
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+    
+    def get_absolute_url(self):
+        return reverse('product:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
-	name = models.CharField(max_length=200)
-	price = models.FloatField()
-	digital = models.BooleanField(default=False,null=True, blank=True)
+	category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
+	created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+	title = models.CharField(max_length=255)
+	author = models.CharField(max_length=255,default='admin')
+	description = models.TextField(blank=True)
+	image = models.ImageField(upload_to='images/')
+	slug = models.SlugField(max_length=255)
+	price = models.DecimalField(max_digits=5, decimal_places=2)
+	in_stock = models.BooleanField(default=True)
+	in_active = models.BooleanField(default=True)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return self.name
+class Meta:
+    verbose_name_plural = 'Products'
+    ordering = ('-created',)
+
+def get_absolute_url(self):
+    return reverse('product:product_detail', args=[self.slug])
+
+def __str__(self):
+    return self.title 
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -47,3 +75,7 @@ class ShippingAddress(models.Model):
 
 	def __str__(self):
 		return self.address
+
+
+
+    
