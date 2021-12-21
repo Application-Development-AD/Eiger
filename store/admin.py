91d9e45372af8b00d1c214772713e188bd9dd073
Nida Damia <nidamianizam@gmail.com>
2021-12-21 09:33:02 +0800

    init

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
-
-from .models import Category, Product
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
