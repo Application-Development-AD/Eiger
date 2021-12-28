a0ccbc0ad7388eb8d9bf4d5bdf9ce83fe251b252
Nida Damia <nidamianizam@gmail.com>
2021-12-28 22:05:06 +0800

    damia

--- store/models.py
+++ store/models.py
@@ -25,28 +25,38 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-	category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
-	created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+	#category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
+	#created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
 	title = models.CharField(max_length=255)
-	author = models.CharField(max_length=255,default='admin')
-	description = models.TextField(blank=True)
-	image = models.ImageField(upload_to='images/')
-	slug = models.SlugField(max_length=255)
+	#author = models.CharField(max_length=255,default='admin')
+	#description = models.TextField(blank=True)
+	image = models.ImageField(null=True, blank=True)#(upload_to='images/')
+	#slug = models.SlugField(max_length=255)
 	price = models.DecimalField(max_digits=5, decimal_places=2)
-	in_stock = models.BooleanField(default=True)
-	in_active = models.BooleanField(default=True)
-	created = models.DateTimeField(auto_now_add=True)
-	updated = models.DateTimeField(auto_now=True)
+	#in_stock = models.BooleanField(default=True)
+	#in_active = models.BooleanField(default=True)
+	#created = models.DateTimeField(auto_now_add=True)
+	#updated = models.DateTimeField(auto_now=True)
+	digital = models.BooleanField(default=False, null=True, blank=False)
 
-class Meta:
-    verbose_name_plural = 'Products'
-    ordering = ('-created',)
+#class Meta:
+   # verbose_name_plural = 'Products'
+   # ordering = ('-created',)
 
-def get_absolute_url(self):
-    return reverse('product:product_detail', args=[self.slug])
-
+#def get_absolute_url(self):
+    #return reverse('product:product_detail', args=[self.slug])
+@property
+def imageURL(self):
+	try:
+		url = self.image.models.url
+	except: 
+		url =''
+	return url
+	
 def __str__(self):
     return self.title 
+
+
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -57,11 +67,28 @@ class Order(models.Model):
 	def __str__(self):
 		return str(self.id)
 
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total
+
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total
+
 class OrderItem(models.Model):
 	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+	@property
+	def get_total(self):
+		total = self.product.price * self.quantity
+		return total
 
 
 class ShippingAddress(models.Model):
