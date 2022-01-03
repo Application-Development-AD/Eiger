72cdc7e556add527fc1a4d3800bb11b522456ab2
Maizatulhaza <maizatulhaza@graduate.utm.my>
2022-01-03 21:35:39 +0800

    changes2

--- store/urls.py
+++ store/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    path('main/', views.main, name="main"),
     path('', views.search, name="search"),
     path('cart/', views.cart, name="cart"),
     path('login/', views.login, name="login"),
     path('forgotpassword/', views.forgotpassword, name="forgotpassword"),
     path('accountDetails/', views.accountDetails, name="accountDetails"),
+    path('settings/', views.settings, name="settings"),
+    path('payments/', views.payments, name="payments"),
 ]
  
--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def main(request):
+    context = {}
+    return render (request, 'store/main.html', context)
+
 def search(request):
     context = {}
     return render (request, 'store/search.html', context)
@@ -21,3 +25,11 @@ def forgotpassword(request):
 def accountDetails(request):
     context = {}
     return render(request, 'store/accountDetails.html', context)
+
+def settings(request):
+    context = {}
+    return render(request, 'store/settings.html', context)
+
+def payments(request):
+    context = {}
+    return render(request, 'store/payments.html', context)
