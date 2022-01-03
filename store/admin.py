52c185bed85352d81e07e6617953a6db6820c1de
Nida Damia <nidamianizam@gmail.com>
2022-01-04 00:45:58 +0800

    init

--- store/admin.py
+++ store/admin.py
@@ -21,15 +21,3 @@ admin.site.register(Product)
  #   list_filter = ['in_stock','in_active']
   #  prepopulated_fields = {'slug':('title',)}
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ['name', 'slug']
-    prepopulated_fields = {'slug':('name',)}
-
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title','author','slug','price',
-                 'in_stock','created','updated']
-    list_filter = ['in_stock','in_active']
-    prepopulated_fields = {'slug':('title',)}
-
