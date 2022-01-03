258fde5266a1e867960e904414fe480ba0603076
Nida Damia <nidamianizam@gmail.com>
2022-01-04 00:44:30 +0800

    init

--- store/models.py
+++ store/models.py
@@ -105,29 +105,3 @@ class ShippingAddress(models.Model):
 
 	def __str__(self):
 		return self.address
-
-    
-class Product(models.Model):
-    category = models.ForeignKey(Category,related_name='product', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255,default='admin')
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
-    slug = models.SlugField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    in_stock = models.BooleanField(default=True)
-    in_active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-class Meta:
-    verbose_name_plural = 'Products'
-    ordering = ('-created',)
-
-#def get_absolute_url(self):
-#    return reverse('product:product_detail', args=[self.slug])
-
-def __str__(self):
-    return self.title 
-
