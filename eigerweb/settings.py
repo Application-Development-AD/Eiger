db8fb74477c1962dc450df40dfb8e30d3c199f4c
Nasrin Sabrina <nasrinsabrina@graduate.utm.my>
2022-01-04 09:38:40 +0800

    update

--- eigerweb/settings.py
+++ eigerweb/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/image/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'/image/')
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'/images/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
