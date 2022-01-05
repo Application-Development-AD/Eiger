b6981c95867a05ac6811abe42cd1f226495095d2
Nasrin Sabrina <nasrinsabrina@graduate.utm.my>
2022-01-05 22:59:10 +0800

    commit home

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Product
 
 # Create your views here.
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products':products})
 
 def search(request):
     context = {}
