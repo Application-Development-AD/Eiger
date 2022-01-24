e74199e182c0995123aa09a1ed7bf38c1136daad
Nida Damia <nidamianizam@gmail.com>
2022-01-24 18:37:40 +0800

    init

--- store/views.py
+++ store/views.py
@@ -3,6 +3,19 @@ from .models import *
 
 # Create your views here.
 
+def main(request):
+    context = {}
+    return render (request, 'store/main.html', context)
+
+def home(request):
+    products = Product.objects.all()
+    context = {'products':products}
+    return render (request, 'store/home.html', context)
+
+def search(request):
+    context = {}
+    return render (request, 'store/search.html', context)
+
 def cart(request):
     context = {}
     return render(request, 'store/cart.html', context)
